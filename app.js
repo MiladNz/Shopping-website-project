6f334e3b7a4eb9f01e3862e3601812e6ef7b8f94
MiladNz <milad.n91@gmail.com>
2021-08-31 01:29:38 +0430

    cart items modal

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdop");
 const closeModal = document.querySelector(".cart-item-confirm");
+
+//cart modal
+function showModalFunction(){
+    backDrop.style.display = "black";
+    cartModal.style.opacity = "1";
+    cartModal.style.top = "20%";
+}
+
+function closeModalFunction(){
+    backDrop.style.display = "none";
+    cartModal.style.opacity = "0";
+    cartModal.style.top = "-100%";
+}
+
+cartBtn.addEventListener("click",showModalFunction);
+closeModal.addEventListener("click", closeModalFunction);
+backDrop.addEventListener("click", closeModalFunction);
