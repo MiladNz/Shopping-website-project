3924de2e0550078966b2ce77f5e5cb29eb8ff9b1
MiladNz <milad.n91@gmail.com>
2021-08-31 00:44:30 +0430

    commit on master

---
+++ app.js
@@ -0,0 +1,4 @@
+const cartBtn = document.querySelector(".cart-btn");
+const cartModal = document.querySelector(".cart");
+const backDrop = document.querySelector(".backdop");
+const closeModal = document.querySelector(".cart-item-confirm");
