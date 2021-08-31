748d22b71d03c411973037d03ba1b0705da89e64
MiladNz <milad.n91@gmail.com>
2021-08-31 23:44:35 +0430

    commit on master

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+import { productsData } from "./products";
 
 const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".cart-item-confirm");
 
+//1.get products from producs.js
+class Products{
 
+}
+//2.display products
+class UI{
+
+}
+//3.storage
+class Storage{
+
+}
+
+//////////////////////////////////////////
 function showModalFunction() {
   backDrop.style.display = "block";
   cartModal.style.opacity = "1";
