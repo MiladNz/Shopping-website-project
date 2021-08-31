351d8ba1df80d54e2647613a954f9de6bf6c034e
MiladNz <milad.n91@gmail.com>
2021-08-31 23:15:08 +0430

    create products file

---
+++ products.js
@@ -0,0 +1,51 @@
+export const productsData = [
+    {
+      id: 1,
+      title: "Queen panel bed",
+      price: 10.99,
+      imageUrl: "./images/product-1.jpeg",
+    },
+    {
+      id: 2,
+      title: "King panel bed",
+      price: 14.99,
+      imageUrl: "./images/product-2.jpeg",
+    },
+    {
+      id: 3,
+      title: "Royal panel bed",
+      price: 7.99,
+      imageUrl: "./images/product-3.jpeg",
+    },
+    {
+      id: 4,
+      title: "Twin panel bed",
+      price: 11.99,
+      imageUrl: "./images/product-4.jpeg",
+    },
+    {
+      id: 5,
+      title: "Fridge",
+      price: 4.99,
+      imageUrl: "./images/product-5.jpeg",
+    },
+    {
+      id: 6,
+      title: "Sofa",
+      price: 23.99,
+      imageUrl: "./images/product-6.jpeg",
+    },
+    {
+      id: 7,
+      title: "Dresser",
+      price: 42.99,
+      imageUrl: "./images/product-7.jpeg",
+    },
+    {
+      id: 8,
+      title: "Desk",
+      price: 24.99,
+      imageUrl: "./images/product-8.jpeg",
+    },
+  ];
+  
