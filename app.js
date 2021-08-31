0e2fcfa98be28c08a5613b76495c5791bbe178d8
MiladNz <milad.n91@gmail.com>
2021-09-01 01:08:07 +0430

    dynamically adding products to DOM

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
-import { productsData } from "./products";
+import { productsData } from "./products.js";
 
 const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".cart-item-confirm");
 
-//1.get products from producs.js
-class Products{
 
+const productsDOM = document.querySelector(".products-center");
+//1.get products from producs.js
+
+class Products{
+  getProduct(){
+    return productsData;
+  }
 }
 //2.display products
-class UI{
 
+class UI{
+  displayProducts(products){
+    let result = "";
+    products.forEach(item => {
+      result +=`<div class="product">
+            <div class="img-container">
+              <img src="${item.imageUrl}" class="product-img" />
+            </div>
+            <div class="product-desc">
+              <p class="product-price">${item.price}</p>
+              <p class="product-title">${item.title}</p>
+            </div>
+            <button class="btn add-to-cart" data-id=${item.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to cart
+            </button>
+          </div>`;
+          productsDOM.innerHTML = result;
+    });
+  }
 }
 //3.storage
-class Storage{
 
+class Storage{
+  
 }
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+  const products = new Products();
+  const productsData = products.getProduct();
+  const ui = new UI();
+  ui.displayProducts(productsData);
+  // console.log(productsData);
+})
+
+
+
+
+
+
+
+
 
 //////////////////////////////////////////
 function showModalFunction() {
