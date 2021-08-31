04368333650224998a6bc32b62beed88dad576f0
MiladNz <milad.n91@gmail.com>
2021-08-31 13:35:19 +0430

    commit on master

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
+
 const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
-const backDrop = document.querySelector(".backdop");
+const backDrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".cart-item-confirm");
 
-//cart modal
-function showModalFunction(){
-    backDrop.style.display = "black";
-    cartModal.style.opacity = "1";
-    cartModal.style.top = "20%";
+
+function showModalFunction() {
+  backDrop.style.display = "block";
+  cartModal.style.opacity = "1";
+  cartModal.style.top = "20%";
 }
 
-function closeModalFunction(){
-    backDrop.style.display = "none";
-    cartModal.style.opacity = "0";
-    cartModal.style.top = "-100%";
+function closeModalFunction() {
+  backDrop.style.display = "none";
+  cartModal.style.opacity = "0";
+  cartModal.style.top = "-100%";
 }
 
-cartBtn.addEventListener("click",showModalFunction);
+cartBtn.addEventListener("click", showModalFunction);
 closeModal.addEventListener("click", closeModalFunction);
 backDrop.addEventListener("click", closeModalFunction);
